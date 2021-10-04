b4226ad16d8b19b987409fc5c0b202b93401348c
plateauh <70834431+plateauh@users.noreply.github.com>
2021-10-04 13:08:35 +0300

    UI done

--- app/src/main/java/com/example/jsonapp/MainActivity.kt
+++ app/src/main/java/com/example/jsonapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.jsonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+
+lateinit var amountEditText: EditText
+lateinit var currencySpinner: Spinner
+lateinit var convertButton: Button
+lateinit var resultTextView: TextView
+lateinit var dateTextView: TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        amountEditText = findViewById(R.id.amount_et)
+
+        // from https://stackoverflow.com/a/17650125
+        currencySpinner = findViewById(R.id.currency_spinner)
+        val currencies = listOf("Choose currency", "", "")
+        currencySpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, currencies)
+
+
     }
 }
